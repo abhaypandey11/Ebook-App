cb43465f9d75d2b5d1c05a9421f06d6ab23774e4
abhaypandey11 <abhaypandey200209@gmail.com>
2025-05-10 12:14:41 +0530

    commit

--- src/main/java/com/user/servlet/RegisterServlet.java
+++ src/main/java/com/user/servlet/RegisterServlet.java
@@ -1,7 +1,61 @@
 package com.user.servlet;
 
-import javax.servlet.http.HttpServlet;
+import java.io.IOException;
 
-public class RegisterServlet extends Httpservlet{
-    // Add doPost or doGet here later
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.DAO.UserDAOimpl;
+import com.DB.DBConnect;
+import com.entity.user;
+
+import javax.servlet.http.HttpSession;
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().println("RegisterServlet working!");
+        try {
+        	String name=req.getParameter("fname");
+        	String email=req.getParameter("email");
+        	String phno=req.getParameter("phno");
+        	String password=req.getParameter("password");
+        	String check=req.getParameter("check");
+
+        	System.out.println(name+"" +email+ "" + phno+ "" + password+ "" +check) ;
+        	user us=new user();
+        	us.setName(name);
+        	us.setEmail(email);
+        	us.setPhno(phno);
+        	us.setPassword(password);
+        	HttpSession session = req.getSession();        	
+        	
+        	if(check!=null) {
+        	UserDAOimpl dao = new UserDAOimpl(DBConnect.getConn());
+        	boolean f=dao.userRegister(us);
+        	if(f) {
+//        		System.out.println("User Register Success");
+        		session.setAttribute("succMsg", "Register successfully..");
+        		response.sendRedirect("register.jsp");
+        	}else {
+//        		System.out.println("something wrong on server");
+        		session.setAttribute("failedMsg", "Something wronf on server..");
+        		response.sendRedirect("register.jsp");
+        	}
+        	}else { 
+//        		System.out.println("please check agree term and condition");
+        		session.setAttribute("failedMsg", "please check agree term and condition");
+        		response.sendRedirect("register.jsp");
+        		}
+        } catch(Exception e) {
+        	e.printStackTrace();
+        	
+        }
+    }
 }
--- src/main/webapp/register.jsp
+++ src/main/webapp/register.jsp
@@ -1,5 +1,8 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
+	<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+	<%@page isELIgnored="false" %>
+	
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,32 +18,41 @@
 				<div class="card">
 					<div class="card-body">
 					
-					<form>
+					<form action="register" method="post">
 	<h4 class="text-center mb-4 text-primary">Registration Page</h4>
+	<c:if test="${not empty succMsg }">
+	<p class="text-center text-success">${succMsg }</p>
+	<c:remove var="succMsg" scope="session"/>
+	</c:if>
+	<c:if test="${not empty failedMsg }">
+	<p class="text-center text-danger">${failedMsg }</p>
+	<c:remove var="failedMsg" scope="session"/>
+	</c:if>
+	
 
 	<div class="mb-3">
 		<label for="fullName" class="form-label">Full Name</label>
-		<input type="text" class="form-control" id="fullName" placeholder=""required="required">
+		<input type="text" class="form-control" id="fullName" placeholder=""required="required" name="fname">
 	</div>
 
 	<div class="mb-3">
 		<label for="email" class="form-label">Email address</label>
-		<input type="email" class="form-control" id="email" placeholder=""required="required">
+		<input type="email" class="form-control" id="email" placeholder=""required="required" name="email">
 		
 	</div>
 
 	<div class="mb-3">
 		<label for="phone" class="form-label">Phone No</label>
-		<input type="number" class="form-control" id="phone" placeholder=""required="required">
+		<input type="number" class="form-control" id="phone" placeholder=""required="required" name="phno">
 	</div>
 
 	<div class="mb-3">
 		<label for="password" class="form-label">Password</label>
-		<input type="password" class="form-control" id="password" placeholder=""required="required">
+		<input type="password" class="form-control" id="password" placeholder=""required="required" name="password">
 	</div>
 
 	<div class="mb-3 form-check">
-		<input type="checkbox" class="form-check-input" id="check">
+		<input type="checkbox" class="form-check-input" id="check" name="check">
 		<label class="form-check-label" for="check">I agree to the terms & conditions</label>
 	</div>
 
